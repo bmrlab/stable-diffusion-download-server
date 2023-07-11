ae3715f330eefda8861cc01972eb8992fa1b340e
lijianan <lijianan@tezign.com>
2023-07-11 11:38:09 +0800

    feat: update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 sudo docker build -t file-downloader .
 
-sudo docker run --name file-downloader -p 8000:8000 -v /opt/data:/app/data -d file-downloader:latest
+sudo docker run --name file-downloader -p 8000:8000 -v /home/azureuser/kang/stable-diffusion-webui-docker:/app/data -d file-downloader:latest
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.responses import FileResponse
 import requests
 import logging
 
@@ -9,13 +10,13 @@ app = FastAPI()
 base_dir = "./data"
 
 
-class DownloadItem(BaseModel):
+class FetchItem(BaseModel):
     url: str
     filepath: str
 
 
-@app.post("/download")
-def read_root(item: DownloadItem):
+@app.post("/fetch")
+def fetch(item: FetchItem):
     r = requests.get(url=item.url)
     logging.info(f"start download: {item}")
     if r.ok:
@@ -24,3 +25,15 @@ def read_root(item: DownloadItem):
         with open(os.path.join(d, os.path.basename(item.filepath)), 'wb') as f:
             f.write(r.content)
     return {"is_ok": True}
+
+
+@app.get("/download/{file_path:path}")
+async def download_file(file_path: str):
+    """
+    文件下载
+    :param file_path:
+    :return:
+    """
+    file_location = os.path.join(base_dir, file_path)
+    filename = os.path.basename(file_location)
+    return FileResponse(file_location, filename=filename)
